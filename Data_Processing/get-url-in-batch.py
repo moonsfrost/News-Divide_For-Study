51b1215616c75802cc4d153f8edee38500c94530
moonsfrost <mydsqwe@foxmail.com>
2022-07-20 17:00:27 +0800

    Update get-url-in-batch.py

--- Data_Processing/get-url-in-batch.py
+++ Data_Processing/get-url-in-batch.py
@@ -1,7 +1,11 @@
 import os
 import sys
+from subprocess import Popen, PIPE
 
 path = input()
 files = os.listdir(path)
+
 for file in files:
-    print(file)
+    location = "Geturl.exe"
+    d = Popen([location],stdin=PIPE,stdout=PIPE,stderr=PIPE, encoding="UTF8")
+    d.stdin.write()
