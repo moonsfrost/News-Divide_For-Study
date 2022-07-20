f07eddad4f311107861d475330fd36190e7f05ec
moonsfrost <mydsqwe@foxmail.com>
2022-07-20 21:04:29 +0800

    Update GetNews.py

--- Data_Processing/GetNews.py
+++ Data_Processing/GetNews.py
@@ -1,15 +1,14 @@
-from pathlib import Path
 from newspaper import Article
 import sys
 
 Url = sys.argv[1]
 path = sys.argv[2]
-file = open(path,'w')
 
-news = Article(Url,lang='zh')
+news = Article(Url,language='zh')
 news.download()
 news.parse()
 
+file = open(path,'w')
 file.write(news.title)
 file.write('\n')
 file.write(news.text)
