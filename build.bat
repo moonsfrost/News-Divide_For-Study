563863842b178d3394850c22b82ecaa8df304047
moonsfrost <mydsqwe@foxmail.com>
2022-07-22 17:12:09 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 g++ .\Data_processing\Geturl.cpp -std=c++14 -Wall -o .\Data_processing\Geturl -static-libgcc -static-libstdc++
 g++ .\Data_processing\UrlToNews.cpp -std=c++14 -Wall -o .\Data_processing\UrlToNews -static-libgcc -static-libstdc++
+g++ .\News_Sort.cpp -std=c++14 -Wall -o .\News_Sort -static-libgcc -static-libstdc++
